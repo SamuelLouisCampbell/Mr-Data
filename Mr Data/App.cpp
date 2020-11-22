e171b39476f449bed3ca311f45dc45e1c7e3172d
Sam Campbell <sam@sclighting.co.uk>
2020-11-22 18:18:52 +0000

    List of useful colours added

--- Mr Data/App.cpp
+++ Mr Data/App.cpp
@@ -16,9 +16,9 @@ App::App()
 	centre.y = float(wnd.GetHeight()) / 2.0f;
 	txt.SetPos(centre);
 	
-	lines.emplace_back(std::make_unique<LineMaker>(wnd.Gfx(), pos_1, pos_2, Red));
-	lines.emplace_back(std::make_unique<LineMaker>(wnd.Gfx(), pos_3, pos_4, Cyan));
-	lines.emplace_back(std::make_unique<BoxOutline>(wnd.Gfx(), pos_5, pos_6, Red));
+	lines.emplace_back(std::make_unique<LineMaker>(wnd.Gfx(), pos_1, pos_2, Colors::Red));
+	lines.emplace_back(std::make_unique<LineMaker>(wnd.Gfx(), pos_3, pos_4, Colors::Orange));
+	lines.emplace_back(std::make_unique<BoxOutline>(wnd.Gfx(), pos_5, pos_6,Colors::Teal));
 	wnd.Gfx().SetProjection(DirectX::XMMatrixOrthographicOffCenterLH(0.0f, float(wnd.Gfx().GetWindowWidth()),
 		float(wnd.Gfx().GetWindowHeight()), 0.0f, 0.0f, 1.0f));
 }
@@ -43,7 +43,7 @@ void App::ComposeFrame()
 {	
 	txt.Bind(wnd.Gfx());
 	
-	Color clearCol = { 0.0f, 0.0f, 0.0f, 1.0f};
+	Color clearCol = { Colors::Black };
 	wnd.Gfx().BeginFrame(clearCol);
 	
 	if (ImGui::Begin("Text Controls"))
--- Mr Data/Color.h
+++ Mr Data/Color.h
@@ -1,18 +1,34 @@
 #pragma once
 #include "BarebonesWindows.h"
 
-struct Color
-{
-	float r = 1.0f;
-	float g = 1.0f;
-	float b = 1.0f;
-	float a = 1.0f;
-};
+	struct Color
+	{
+		float r = 1.0f;
+		float g = 1.0f;
+		float b = 1.0f;
+		float a = 1.0f;
+	};
 
-struct ColorChar
+	struct ColorChar
+	{
+		unsigned char b;
+		unsigned char g;
+		unsigned char r;
+		unsigned char a;
+	};
+
+namespace Colors
 {
-	unsigned char b;
-	unsigned char g;
-	unsigned char r;
-	unsigned char a;
-};
+	static Color Red =		{ 1.0f, 0.0f, 0.0f, 1.0f };
+	static Color Green =	{ 0.0f, 1.0f, 0.0f, 1.0f };
+	static Color Blue =		{ 0.0f, 0.0f, 1.0f, 1.0f };
+	static Color Cyan =		{ 0.0f, 1.0f, 1.0f, 1.0f };
+	static Color Magenta =	{ 1.0f, 0.0f, 1.0f, 1.0f };
+	static Color Yellow =	{ 1.0f, 1.0f, 0.0f, 1.0f };
+	static Color Teal =		{ 0.0f, 1.0f, 0.5f, 1.0f };
+	static Color Orange =	{ 1.0f, 0.5f, 0.0f, 1.0f };
+	static Color Purple =	{ 0.5f, 0.0f, 1.0f, 1.0f };
+	static Color Grey =		{ 0.5f, 0.5f, 0.5f, 1.0f };
+	static Color White =	{ 1.0f, 1.0f, 0.0f, 1.0f };
+	static Color Black =	{ 0.0f, 0.0f, 0.0f, 1.0f };
+}
